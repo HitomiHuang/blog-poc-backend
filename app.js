ce40690be774a51c775bec5904dff980f7340be9
HitomiHuang <black011602@gmail.com>
2022-12-04 22:07:19 +0800

    sequelize init

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const routes = require('./routes')
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.use('/api', routes)
 
 app.listen(port, () => {
-  console.log('App is running on http://localhost:3000')
+  console.log(`App is running on http://localhost:${port}`)
 })
